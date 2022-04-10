26cf9fb605011f1ef87b2003aa64ab6543458cfe
Theodorus Clarence <theodorusclarence@gmail.com>
2022-04-10 16:22:19 +0700

    feat: add difficulty and picker for new task

--- mc1-team-10/Views/Add Task/AddTaskViewController.swift
+++ mc1-team-10/Views/Add Task/AddTaskViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class AddTaskViewController: UIViewController {
 
+    @IBOutlet weak var navBar: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(cancelModal))
+        
+        navBar.topItem?.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .done, target: self, action: nil)
+    }
+    
+    @objc func cancelModal() {
+        dismiss(animated: true)
     }
     
     
